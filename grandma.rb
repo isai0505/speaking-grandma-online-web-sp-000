935f342983c47ce328882f77feb17c7141d19f06
Isai Bismark Cortez <isai0505@gmail.com>
2019-07-22 03:00:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(speak)
   if speak == speak.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif speak == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif speak == speak.upcase
     return "NO, NOT SINCE 1938!"
   end
